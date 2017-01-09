ed71ac0ba37e70861e7f94ce2ee338ca4751f80c
Celoxocis <celox.ocis@gmail.com>
2017-01-09 11:38:09 +0000

    Initialize repository for Lenovo P2 (P2a42)
    Signed-off-by: Celoxocis <celox.ocis@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,116 @@
+DEVICE_TREE := device/lenovo/P2a42
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8953
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
+#TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+
+# Flags
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+#COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Kernel
+#TARGET_KERNEL_SOURCE := kernel/lenovo/msm8953
+#TARGET_KERNEL_ARCH := arm64
+#TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGET_KERNEL_CONFIG := twrp_defconfig
+#TARGET_KERNEL_DEVICE_DEFCONFIG := device_P2a42
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
+
+# Boot image
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci cma=32M@0-0xffffffff androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_OFFSET = 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864        #    65536 * 1024 mmcblk0p39
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864    #    65536 * 1024 mmcblk0p40
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4294967296    #  4194304 * 1024 mmcblk0p54
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 25631374336 # 25030639 * 1024 mmcblk0p56
+BOARD_CACHEIMAGE_PARTITION_SIZE := 318767104      #   311296 * 1024 mmcblk0p53
+BOARD_FLASH_BLOCK_SIZE := 131072                  # (BOARD_KERNEL_PAGESIZE * 64)
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 149
+TW_SCREEN_BLANK_ON_BOOT := true
+
+# TWRP Additional Flags
+RECOVERY_VARIANT := twrp
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_USB_STORAGE := true
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+#TW_INPUT_BLACKLIST := "hbtp_vm"
+# disabled in twrp_defconfig kernel
+
+# TWRP Overlay Graphics
+TW_NEW_ION_HEAP := true
+TW_TARGET_USES_QCOM_BSP := true
+TARGET_SPECIFIC_HEADER_PATH := device/lenovo/P2a42/include
+
+# exFAT drivers included in the kernel?
+TW_NO_EXFAT_FUSE := true
+
+# No love for the wicked (device ships with M)
+TW_EXCLUDE_SUPERSU := true
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := false
+# disabled due to read failure
+
+# For TWRP Touchscreen Event Logging
+TWRP_EVENT_LOGGING := false
+# adb shell tail -f /tmp/recovery.log
+
+# Time Zone data
+PRODUCT_COPY_FILES += \
+bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+# Additional alternative flags
+#TW_USE_TOOLBOX := true
+# don't take forever to wipe
+#COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
---
+++ README.md
@@ -0,0 +1,31 @@
+## TWRP device tree for Lenovo P2 (P2a42)
+
+Add to `.repo/local_manifests/P2a42.xml`:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+ <remote  name="Celoxocis"
+           fetch="https://github.com/Celoxocis" />
+ <remote  name="omnirom"
+           fetch="https://github.com/omnirom" />
+ <remote  name="CyanogenMod"
+           fetch="https://github.com/CyanogenMod" />
+    <project path="device/lenovo/P2a42" name="android_device_lenovo_P2a42" remote="Celoxocis" revision="android-6.0" />
+    <project path="bootable/recovery-twrp" name="android_bootable_recovery" remote="omnirom" revision="android-7.1" />
+    <project path="external/busybox" name="android_external_busybox" remote="omnirom" revision="android-7.1" />
+    <remove-project path="external/f2fs-tools" name="CyanogenMod/android_external_f2fs-tools" groups="pdk" />
+    <project path="external/f2fs-tools" name="android_external_f2fs-tools" remote="omnirom" revision="android-7.1" />
+    <project path="device/qcom/common" name="android_device_qcom_common" remote="CyanogenMod" revision="cm-13.0" />
+</manifest>
+```
+
+Then run `repo sync` to check it out.
+
+To build:
+
+```sh
+. build/envsetup.sh
+lunch cm_P2a42-eng
+make -j1 recoveryimage
+```
